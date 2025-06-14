942cb6b0e3e73a78a36bd2eb5855133441d3f5eb
JayaprabhuD-cloud <mail2jayaprabhu@gmail.com>
2025-06-14 16:13:54 +0530

    fixing the issues

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
   terraform {
   backend "s3" {
-    bucket = "terraform-remote-st"
-    key    = "eks-demo/terraform.tfstate"
+    bucket = "terraform-remote-state-bucket-s3-hcl"
+    key    = "usecase-9/terraform.tfstate"
     region = "us-east-1" 
  
     # For State Locking
-    dynamodb_table = "terraform-locks"    
+    dynamodb_table = "dynamodb-state-lock-hcl"    
   } 
   } 
